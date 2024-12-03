06b8cb916c9a30298506f812a2c9adff2e33f573
SamyMechiche <mechiche.samysm@gmail.com>
2024-12-03 16:42:15 +0100

    change css path and made the site more responsive

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,3 +7,31 @@ burgerMenuButton.onclick = function(){
     const isOpen = burgerMenu.classList.contains('open')
     burgerMenuIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
 }
+
+const leftArrow = document.getElementsByClassName('arrow-left');
+const rightArrow = document.getElementsByClassName('arrow-right');
+const slides = document.querySelector('.slides')
+const listSlides = document.getElementsByClassName('slide');
+let nbSlides = listSlides.length;
+let activeSlide = 0;
+
+function setMarge(button, max, step, frame){
+    if(button == 'left'){
+        activeSlide--;
+        activeSlide = activeSlide < 0 ? 0 : activeSlide;
+    } else {
+        activeSlide++
+        activeSlide = activeSlide > max - 1 ? max - 1 : activeSlide
+    }
+
+    let margeLeft = step * activeSlide;
+    frame.style.marginLeft = -margeLeft + 'px';
+}
+
+leftArrow[0].addEventListener('click', function(){
+    setMarge('left', nbSlides, 500, slides)
+});
+
+rightArrow[0].addEventListener('click', function(){
+    setMarge('right', nbSlides, 500, slides)
+});
